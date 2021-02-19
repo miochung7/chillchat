b2363caec521410267bb9adc632e4f367abb0e1d
Bianca Iordache <biannca.iordache@gmail.com>
2021-02-19 16:28:42 +0000

    target folder updates

--- src/main/java/com/example/emojify/controller/TemplateController.java
+++ src/main/java/com/example/emojify/controller/TemplateController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class TemplateController {
 
     @GetMapping("/")
@@ -28,4 +29,5 @@ public class TemplateController {
     public String getSpotify() {
         return "spotify";
     }
+
 }
